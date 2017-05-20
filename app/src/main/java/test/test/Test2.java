acdf31fc3782cd39a7a240c7d8c6ded652f65700
aclear <1542218540@qq.com>
2017-05-20 16:42:56 +0800

    Test

--- app/src/main/java/test/test/Test2.java
+++ app/src/main/java/test/test/Test2.java
@@ -1,6 +1,21 @@
 package test.test;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import dalvik.system.DexClassLoader;
 
@@ -12,11 +27,107 @@ import dalvik.system.DexClassLoader;
 public class Test2 {
 
     public static void main(String args[]) throws Exception {
-        String dexPath = "F:/classes.jar";
-        String direct = "F:/opt";
-        DexClassLoader loader = new DexClassLoader(dexPath,direct,null,ClassLoader.getSystemClassLoader());
-        Class<?> mClass =loader.loadClass("test.testlib.TestLib");
-        Method add = mClass.getDeclaredMethod("add",int.class,int.class);
-        System.out.println(add.invoke(1,2));
+//        String dexPath = "F:/classes.jar";
+//        String direct = "F:/opt";
+//        DexClassLoader loader = new DexClassLoader(dexPath,direct,null,ClassLoader.getSystemClassLoader());
+//        Class<?> mClass =loader.loadClass("test.testlib.TestLib");
+//        Method add = mClass.getDeclaredMethod("add",int.class,int.class);
+//        System.out.println(add.invoke(1,2));
+
+
+
+//        loadBitmap();
+//        copyFile();
+        copyText();
     }
+
+
+    private static void loadBitmap() throws Exception {
+        String path ="https://ubmcmm.baidustatic.com/media/v1/0f000PHTmuKhFYwAov0ogf.gif";
+        File file = new File("F:/aaa.jpg");
+        FileOutputStream fos = new FileOutputStream(file);
+        URL url = new URL(path);
+        URLConnection conn = url.openConnection();
+        conn.setReadTimeout(5000);
+        conn.setConnectTimeout(5000);
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        conn.connect();
+        InputStream is = conn.getInputStream();
+        byte data[] = new byte[1024];
+        int len;
+        while ((len = is.read(data))!=-1){
+            fos.write(data,0,len);
+        }
+        fos.flush();
+
+        is.close();
+        fos.close();
+
+
+    }
+
+    private static void copyFile(){
+        BufferedOutputStream bos = null;
+        BufferedInputStream bis = null;
+        try {
+            File output = new File("F:/output.exe");
+            File input = new File("F:/Git-2.12.2.2-64-bit.exe");
+            bis = new BufferedInputStream(new FileInputStream(input));
+            bos = new BufferedOutputStream(new FileOutputStream(output));
+
+            int len = 0;
+            byte data[] = new byte[2048];
+            while ((len = bis.read(data))!=-1){
+                bos.write(data,0,len);
+            }
+            bos.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                bis.close();
+                bos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void copyText(){
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        File output = new File("F:/output.txt");
+        File input = new File("F:/新建文本文档.txt");
+
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(input),"GBK"));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output),"GBK"));
+
+            String length = null;
+            while ((length = br.readLine())!=null){
+                bw.write(length);
+                bw.newLine();
+//
+            }
+            bw.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+            try {
+                br.close();
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
 }
--- testlib/src/main/java/test/testlib/TestClass.java
+++ testlib/src/main/java/test/testlib/TestClass.java
@@ -1,15 +1,27 @@
 package test.testlib;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by admin on 2017/5/19.
@@ -21,19 +33,19 @@ public class TestClass {
 
     public View showT(final Context ctx){
         FrameLayout frameLayout = new FrameLayout(ctx);
+        frameLayout.setBackgroundColor(Color.parseColor("#000000"));
         TextView tv = new TextView(ctx);
-        FrameLayout.LayoutParams params1 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams params1 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         int with = ctx.getResources().getDisplayMetrics().widthPixels;
-        Log.d(TAG,"width ------- >>>>  "+with);
         params1.width = with;
+        params1.height = 250;
+        params1.leftMargin = 125;
         tv.setLayoutParams(params1);
         tv.setGravity(Gravity.CENTER);
         tv.setPadding(20,50,20,50);
-        tv.setBackgroundColor(Color.parseColor("#000000"));
         tv.setTextColor(Color.parseColor("#ffffffff"));
         tv.setText("TextClass");
         tv.setFocusable(true);
-        Log.d(TAG,"width  after------- >>>>  "+ tv.getLayoutParams().width);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,6 +89,9 @@ public class TestClass {
                 WindowManager.LayoutParams.WRAP_CONTENT,type,flag, PixelFormat.TRANSLUCENT);
         params.gravity = Gravity.BOTTOM;
 
+
+        loadImage(ctx,frameLayout);
+
         frameLayout.requestFocus();
         manager.addView(frameLayout,params);
 
@@ -93,6 +108,60 @@ public class TestClass {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+
+    private void loadImage(final Context ctx,FrameLayout parent) {
+        try {
+        ImageView iv = new ImageView(ctx);
+        FrameLayout.LayoutParams params  = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        params.gravity = Gravity.LEFT|Gravity.CENTER_VERTICAL;
+        iv.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        iv.setLayoutParams(params);
+        getBitmap(iv);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ctx,"Image Click",Toast.LENGTH_SHORT).show();
+            }
+        });
+        parent.addView(iv);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    private void getBitmap(final ImageView iv) throws Exception{
+
+        final String path ="https://ubmcmm.baidustatic.com/media/v1/0f000PHTmuKhFYwAov0ogf.gif";
+        new AsyncTask<String, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(String... params) {
+
+                Bitmap bitmap = null;
+                try {
+                    URL mUrl = new URL(path);
+                    InputStream is = mUrl.openStream();
+                    bitmap = BitmapFactory.decodeStream(is);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return bitmap;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                iv.setImageBitmap(bitmap);
+            }
+        }.execute("");
+
+
 
     }
 
